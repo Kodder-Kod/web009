757df7cf6c3a7b0013db4a9b19c005c2ef70e723
kod <man364446@gmail.com>
2024-09-06 07:57:48 -0700

    juin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Index from "./views/Index.js";
+import LandingPage from "./views/examples/LandingPage.js";
+import RegisterPage from "./views/examples/RegisterPage.js";
+import ProfilePage from "./views/examples/ProfilePage.js";
+
+
+import { createPath  ,BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/components" element={<Index />} />
+      <Route path="/landing-page" element={<LandingPage />} />
+      <Route path="/register-page" element={<RegisterPage />} />
+      <Route path="/profile-page" element={<ProfilePage />} />
+      <Route path="*" element={<Navigate to="/components" replace />} />
+    </Routes>
+  </BrowserRouter>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+import "./assets/css/nucleo-icons.css";
+import "./assets/scss/blk-design-system-react.scss";
+import "./assets/demo/demo.css";
+
+
+import Index from "./views/Index.js";
+import LandingPage from "./views/examples/LandingPage.js";
+import RegisterPage from "./views/examples/RegisterPage.js";
+import ProfilePage from "./views/examples/ProfilePage.js";
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+<App/>
   </React.StrictMode>
 );
 
